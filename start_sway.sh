c0e289dd08f728467ffe873aa8740d51024d2fed
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-07-21 09:52:25 +0100

    Minor updates

--- start_sway.sh
+++ start_sway.sh
@@ -1,13 +1,15 @@
+#!/bin/sh
+
 # startup script for sway from console
 export GTK_THEME=Dracula
 export QT_QPA_PLATFORMTHEME=qt5ct
 export XDG_CURRENT_DESKTOP=Unity
 export MOZ_ENABLE_WAYLAND=1
 export XDG_SESSION_TYPE=wayland
-XDG_CURRENT_DESKTOP=sway
+export XDG_CURRENT_DESKTOP=sway
 
 # see https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
 [[ $( hostname ) == nina ]] && export WLR_NO_HARDWARE_CURSORS=1
+export WLR_NO_HARDWARE_CURSORS=1
 
 exec dbus-launch --sh-syntax --exit-with-session sway 
-#exec dbus-run-session sway
