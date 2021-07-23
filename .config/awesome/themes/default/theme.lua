fb8723fb94badcde5679d82386434b0983cb16ea
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-07-23 10:01:26 +0100

    Changed Awesome font

--- .config/awesome/themes/default/theme.lua
+++ .config/awesome/themes/default/theme.lua
@@ -8,7 +8,7 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font          = "Fira Code Medium 9"
+theme.font          = "Source Code Pro Medium 9"
 
 theme.bg_normal     = "#222222"
 theme.bg_focus      = "#535d6c"
