9bd163fa9a0906cb36fd705214fe3979843c4ee1
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-08-14 08:26:22 +0100

    Tweaking sway

--- start_sway.sh
+++ start_sway.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
-export LIBSEAT_BACKEND=logind
-export XDG_SESSION_TYPE=wayland
-export XDG_SESSION_DESKTOP=sway
-export XDG_CURRENT_DESKTOP=sway
-export DESKTOP_SESSION=sway
+export EDITOR=nvim
 
 export GTK_THEME=Dracula
-export QT_QPA_PLATFORMTHEME=qt5ct
+
+# Most pure GTK3 apps use wayland by default, but some,
+# like Firefox, need the backend to be explicitely selected.
 export MOZ_ENABLE_WAYLAND=1
+export MOZ_DBUS_REMOTE=1
+export GTK_CSD=0
+
+# qt wayland
+export QT_QPA_PLATFORM="wayland"
+export QT_QPA_PLATFORMTHEME=qt5ct
+export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+
+#Java XWayland blank screens fix
+_JAVA_AWT_WM_NONREPARENTING=1
 
 # see https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
 export WLR_NO_HARDWARE_CURSORS=1
