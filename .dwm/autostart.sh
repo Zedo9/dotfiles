6230a8c180f40edd4cfd84b4f84964031132eae3
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-10-02 21:02:05 +0100

    Disabled skippy-xd and updated wallpaper setter

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-# lxpolkit &
 picom -CGb
-# Fix java apps
-export AWT_TOOLKIT=MToolkit
-export _JAVA_AWT_WM_NONREPARENTING=1
 numlockx on
 dwmblocks &
 cbatticon &
-# volumeicon &
-skippy-xd --start-daemon &
+# skippy-xd --start-daemon &
 dunst &
-sh ~/dotfiles/bin/wallpaper.sh
+wallpaper
 nm-applet &
-#ulauncher --hide-window --no-window-shadow &
