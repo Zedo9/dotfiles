33d71d8bba9602957ef4e5d214ec86155290cf14
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-12-24 13:25:38 +0100

    Added Xresources

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -6,7 +6,7 @@ BOLD=$(tput bold)
 directory=`pwd`
 folders=`ls -a ${directory}/.config`
 #TODO : Make dynamic
-files=".xinitrc .xprofile .bashrc .gitconfig .bash_aliases .screenlayout start_sway.sh .dwm"
+files=".xinitrc .xprofile .bashrc .gitconfig .bash_aliases .screenlayout .dwm .Xresources"
 
 # $1 source - $2 destination
 symlinkFile(){
