d1cd10ad4b64ebb6b03d675a9404516a46bef0b4
Chedly Zouche <chedly.zouche@pm.me>
2022-10-27 10:36:24 +0100

    Update utility scripts

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -6,8 +6,10 @@ export PATH=$PATH:$HOME/dotfiles/scripts/bin
 x-custom-gamma
 dwmblocks &
 cbatticon &
-# skippy-xd --start-daemon &
 dunst &
 wallpaper
 nm-applet &
 x-night-light &
+
+# Faster keyboard rate
+xset r rate 200 30 &
