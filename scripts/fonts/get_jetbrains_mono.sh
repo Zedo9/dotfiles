87d75bca3a22a04064fcd035afc3baf7b0bada89
Chedly Zouche <chedly.zouche@pm.me>
2023-03-19 12:35:29 +0100

    Cleanup scripts

--- scripts/fonts/get_jetbrains_mono.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Install Jetbrains mono
-sudo wget https://download.jetbrains.com/fonts/JetBrainsMono-2.225.zip
-sudo unzip JetBrainsMono-2.225.zip
-sudo mv fonts/ttf/* /usr/share/fonts/truetype/jetbrains-mono/
-sudo fc-cache -fv
-sudo rm -rf JetBrainsMono-2.225.zip OFL.txt AUTHORS.txt fonts
--- scripts/fonts/get_monaco.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/.local/share/fonts/Monaco && \
-curl -L https://raw.githubusercontent.com/rjorge99/Monaco-Fonts/main/Linux/LigaMonacoLinux.ttf
---output ~/.local/share/fonts/Monaco/'LigaMonaco.ttf'
