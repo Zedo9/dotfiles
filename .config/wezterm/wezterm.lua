884ae4408738eb3ce5e49602989ffd2a36e40a96
Chedly Zouche <chedly.zouche@pm.me>
2023-08-05 13:02:37 +0100

    Fix wezterm padding

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,19 +9,27 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.font_size = 20
+config.font_size = 19
 
 config.font = wezterm.font_with_fallback({
-	"CaskaydiaCove Nerd Font",
+	"JetBrainsMono Nerd Font",
 	"Monospace",
 })
 -- For example, changing the color scheme:
 -- config.color_scheme = "Hacktober"
 
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.85
 
+config.color_scheme = 'iTerm2 Default'
+-- config.color_scheme = 'Windows NT (base16)'
 config.default_prog = { "/bin/zsh" }
 
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "NONE"
