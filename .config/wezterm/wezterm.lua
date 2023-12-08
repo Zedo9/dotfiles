823c273f6beca7fe3878ccb130d37046062626d2
Chedly Zouche <chedly.zouche@pm.me>
2023-12-08 19:34:27 +0100

    Term settings updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,24 +1,29 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.font_size = 19
+config.font_size = 15
 config.term = "wezterm"
-config.font = wezterm.font_with_fallback({
-	{ family = "JetBrainsMono Nerd Font", weight = "Medium" },
-	"Monospace",
-})
 
-config.window_background_opacity = 0.7
+config.font = wezterm.font("Cascadia Code", { weight = 450 })
+-- config.font = wezterm.font("Monaco", { weight = 600 })
+-- config.font = wezterm.font("Monaspace Radon", { weight = 500 })
+
+config.window_background_opacity = 0.8
 config.default_prog = { "/bin/zsh" }
-	config.color_scheme = "Dark Pastel"
+config.color_scheme = "Dark Pastel"
+config.tab_bar_at_bottom = true
+
+config.keys = {
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+}
 
 config.window_padding = {
 	left = 0,
@@ -26,7 +31,9 @@ config.window_padding = {
 	top = 0,
 	bottom = 0,
 }
+
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
+
 return config
