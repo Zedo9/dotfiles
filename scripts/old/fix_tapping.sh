fc6ad3fec825227e5ca7d95d071384d01363d2bc
Chedly Zouche <chedly.zouche@pm.me>
2024-10-21 01:27:28 +0200

    Move xorg scripts

--- scripts/old/fix_tapping.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#Deprecated ways
-#tap_id=`xinput --list-props "Elan Touchpad" | grep "Tapping Enabled (" | cut -c28-30`
-#xinput set-prop "Elan Touchpad" $tap_id 1
-xinput set-prop "Elan Touchpad" "libinput Tapping Enabled" 1
